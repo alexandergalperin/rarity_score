e6605488697ca9ffc6c2a29ee0f86d12917a9ca5
Alex <alexander.galperin@gmail.com>
2024-01-29 22:23:14 +0100

    small fix

--- src/mean_method.py
+++ src/mean_method.py
@@ -1,24 +1,18 @@
 import numpy as np
-from tqdm import tqdm
 
 # distances are the calculated distances of the data points
 distances = []
 
 # here you can set the threshold value n_neighbours
-n_neighbours = 100
+n_neighbours = 200
 
 # sort the scores and take the first n_neighbours values
 sorted_distances = [sorted(distance_array)[:n_neighbours] for distance_array in distances]
 
-# calculate the sum of values and the number of instances in each array
-value_sums = [sum(distance_array) for distance_array in sorted_distances]
-instance_counts = [len(distance_array) for distance_array in sorted_distances]
-
 # calculate the average for each array
-averages = [value_sum / instance_count for value_sum, instance_count in zip(value_sums, instance_counts)]
+averages = [sum(score_array) / n_neighbours for score_array in sorted_distances]
 
 # normalization and transformation
-rarity_score = averages
-min_score = min(rarity_score)
-max_score = max(rarity_score)
-rarity_score = (rarity_score - min_score) / (max_score - min_score)
+min_score = min(averages)
+max_score = max(averages)
+rarity_score = [(average - min_score) / (max_score - min_score) for average in averages]
