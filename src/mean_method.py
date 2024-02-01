ea7a890dab4f4539a784603dc14e454b5b75f8a5
Alex <alexander.galperin@gmail.com>
2024-02-01 15:27:04 +0100

    fixed mean

--- src/mean_method.py
+++ src/mean_method.py
@@ -1,18 +1,21 @@
 import numpy as np
 
 def calculate_rarity_scores(distances, n_neighbours):
-    # Ensure distances is a list of numpy arrays for consistent processing
     distances = [np.array(distance_array) for distance_array in distances]
     
-    # sort the scores and take the first n_neighbours values
     sorted_distances = [np.sort(distance_array)[:n_neighbours] for distance_array in distances]
 
-    # calculate the average for each array
     averages = [np.mean(score_array) for score_array in sorted_distances]
 
-    # normalization and transformation
     min_score = min(averages)
     max_score = max(averages)
-    rarity_score = [(average - min_score) / (max_score - min_score) for average in averages]
+
+    # Check if max_score equals min_score to prevent division by zero
+    if max_score == min_score:
+        # If all scores are the same, rarity scores should be 0 or another constant value
+        rarity_score = [0 for _ in averages]  # Here, we choose 0 but you might choose another logic
+    else:
+        rarity_score = [(average - min_score) / (max_score - min_score) for average in averages]
     
     return rarity_score
+
