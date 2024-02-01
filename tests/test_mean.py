1532dcb3ec4fb58b233e2bd8b325317a86635d1f
Alex <alexander.galperin@gmail.com>
2024-02-01 14:47:29 +0100

    new test

---
+++ tests/test_mean.py
@@ -0,0 +1,30 @@
+import pytest
+import numpy as np
+from rarity_score.src.mean_method import calculate_rarity_scores  
+
+@pytest.fixture
+def euclidean_distances():
+    # Generate a sample distance matrix using euclidean distance
+    data = np.random.rand(10, 2)  # 10 data points in 2D space
+    dist_matrix = np.sqrt(((data[:, np.newaxis, :] - data[np.newaxis, :, :]) ** 2).sum(axis=2))
+    return dist_matrix
+
+@pytest.fixture
+def cosine_distances():
+    # Generate a sample distance matrix using cosine distance
+    data = np.random.rand(10, 2)  # 10 data points in 2D space
+    norm_data = data / np.linalg.norm(data, axis=1, keepdims=True)
+    dist_matrix = 1 - np.dot(norm_data, norm_data.T)
+    return dist_matrix
+
+def test_rarity_scores_with_euclidean(euclidean_distances):
+    n_neighbours = 5
+    rarity_scores = calculate_rarity_scores([euclidean_distances], n_neighbours)
+    assert len(rarity_scores) == 1, "There should be one rarity score array"
+    assert 0 <= min(rarity_scores[0]) <= 1, "Rarity scores should be normalized between 0 and 1"
+
+def test_rarity_scores_with_cosine(cosine_distances):
+    n_neighbours = 5
+    rarity_scores = calculate_rarity_scores([cosine_distances], n_neighbours)
+    assert len(rarity_scores) == 1, "There should be one rarity score array"
+    assert 0 <= min(rarity_scores[0]) <= 1, "Rarity scores should be normalized between 0 and 1"
